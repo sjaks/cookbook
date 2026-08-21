284084e67e9f7549c5ce244ef2364da6990ab48b
Sami Jakonen <sami.jakonen@iki.fi>
2026-08-21 09:50:34 +0300

    Use one list for lunch and dinner

--- plan/data/meals.js
+++ plan/data/meals.js
@@ -28,31 +28,12 @@ const MEAL_OPTIONS = {
     { nimi: "Ruisburger ja porkkanat", kcal: 550, proteiini: 25 }
   ],
 
-  paivallinen: [
-    { nimi: "Kasviscurry", kcal: 400, proteiini: 25 },
-    { nimi: "Kalaton salaatti", kcal: 480, proteiini: 30, favorite: true },
-    { nimi: "Uunisalaatti", kcal: 345, proteiini: 14 },
-    { nimi: "Tortillat", kcal: 435, proteiini: 28, favorite: true },
-    { nimi: "Wok", kcal: 335, proteiini: 24, favorite: true },
-    { nimi: "Kikhernepasta", kcal: 385, proteiini: 24 },
-    { nimi: "Halloumibowl", kcal: 425, proteiini: 25 },
-    { nimi: "Nuudelit", kcal: 400, proteiini: 25 },
-    { nimi: "Kanaton keitto", kcal: 265, proteiini: 18, favorite: true },
-    { nimi: "Kalaton keitto", kcal: 380, proteiini: 15 },
-    { nimi: "Makkarakeitto", kcal: 225, proteiini: 16 },
-    { nimi: "Täytetyt paprikat", kcal: 375, proteiini: 26 },
-    { nimi: "Sitruunaiset linssit ja muusi", kcal: 275, proteiini: 18 },
-    { nimi: "Pinaattipannari", kcal: 535, proteiini: 23 },
-    { nimi: "Paras salaatti", kcal: 475, proteiini: 26 },
-    { nimi: "Jauhisdillat", kcal: 580, proteiini: 20 },
-    { nimi: "Kanttarellipiirakka", kcal: 480, proteiini: 18 },
-    { nimi: "Pinaattipiirakka", kcal: 500, proteiini: 15 },
-    { nimi: "Aasiatacot", kcal: 530, proteiini: 15 },
-    { nimi: "Ruisburger ja porkkanat", kcal: 550, proteiini: 25 }
-  ],
+  paivallinen: null,
 
   iltapala: [
     { nimi: "Ruisleivät kasviksilla ja jogurtti marjoilla", kcal: 370, proteiini: 27, favorite: true },
     { nimi: "Sämpylä kasviksilla ja jogurtti marjoilla", kcal: 480, proteiini: 28 }
   ]
 };
+
+MEAL_OPTIONS.paivallinen = MEAL_OPTIONS.lounas;
